ca4e0208d8a63bd177411e79e8f428416831f604
Ty Overby <ty@pre-alpha.com>
2016-03-08 21:22:53 -0800

    minor api changes

--- src/cache/mod.rs
+++ src/cache/mod.rs
@@ -1,19 +1,15 @@
 use std::collections::HashMap;
-use std::boxed::Box;
-
 use super::rasterize::{Font, Bitmap};
 
-
-struct FaceCache<T, E> {
+pub struct FaceCache<T> {
     font: Font,
     atlas: T,
     missing: HashMap<char, T>,
-    trx: Box<Fn(Bitmap) -> Result<T, E>>,
 }
 
-impl <T, E> FaceCache<T, E> {
-    fn new<I, F>(font: Font, chars: I, f: F) -> Result<FaceCache<T, E>, E>
-    where I: Iterator<Item=char>, F: Fn(Bitmap) -> Result<T, E> + 'static
+impl <T> FaceCache<T> {
+    fn new<I, F, E>(font: Font, chars: I, f: F) -> Result<FaceCache<T>, E>
+    where I: Iterator<Item=char>, F: Fn(Bitmap) -> Result<T, E>
     {
             let atlas_bm = font.make_atlas(chars, 20.0, 1, 256, 256);
             let atlas = try!(f(atlas_bm));
@@ -21,7 +17,6 @@ impl <T, E> FaceCache<T, E> {
                 font: font,
                 atlas: atlas,
                 missing: HashMap::new(),
-                trx: Box::new(f)
             })
     }
 }
